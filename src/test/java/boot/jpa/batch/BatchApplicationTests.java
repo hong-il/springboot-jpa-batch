e0a121801ff5dac85513c6a8236d3dd33d743d1d
KIM HONGIL <clroyan01@gmail.com>
2019-12-24 16:29:56 +0900

    Create batch project

---
+++ src/test/java/boot/jpa/batch/BatchApplicationTests.java
@@ -0,0 +1,13 @@
+package boot.jpa.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
