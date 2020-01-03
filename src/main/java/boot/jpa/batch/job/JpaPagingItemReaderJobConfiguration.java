9e573d7b254c1338c234d5463c001e1fc73bf93f
KIM HONGIL <clroyan01@gmail.com>
2020-01-03 22:37:41 +0900

    Create configuration - jpa paging

---
+++ src/main/java/boot/jpa/batch/job/JpaPagingItemReaderJobConfiguration.java
@@ -0,0 +1,71 @@
+package boot.jpa.batch.job;
+
+import boot.jpa.batch.domain.Hero;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManagerFactory;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class JpaPagingItemReaderJobConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final EntityManagerFactory entityManagerFactory;
+
+    private int chunkSize = 10;
+
+    /**How to run this job only
+     * 1. shift + shift
+     * 2. Search Edit Configurations
+     * 3. BatchApplication
+     * 4. Environment
+     * 5. Program arguments : --job.name=jpaPagingItemReaderJob
+     * */
+    @Bean
+    public Job jpaPagingItemReaderJob() {
+        return jobBuilderFactory.get("jpaPagingItemReaderJob")
+                .start(jpaPagingItemReaderStep())
+                .build();
+    }
+
+    /**Unlike hibernate, JPA does not support cursor-based database access.
+     * */
+    @Bean
+    public Step jpaPagingItemReaderStep() {
+        return stepBuilderFactory.get("jpaPagingItemReaderStep")
+                .<Hero, Hero> chunk(chunkSize)
+                .reader(jpaPagingItemReader())
+                .writer(jpaPagingItemWriter())
+                .build();
+    }
+
+    @Bean
+    public JpaPagingItemReader<Hero> jpaPagingItemReader() {
+        return new JpaPagingItemReaderBuilder<Hero>()
+                .name("JpaPagingItemReader")
+                .entityManagerFactory(entityManagerFactory)
+                .pageSize(chunkSize)
+                .queryString("SELECT h FROM Hero h WHERE age >= 26")
+                .build();
+    }
+
+    private ItemWriter<Hero> jpaPagingItemWriter() {
+        return list -> {
+            for (Hero hero : list) {
+                log.info("Current Hero={}", hero);
+            }
+        };
+    }
+}
--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,3 +1,3 @@
+insert into HERO (name, age, created_date) VALUES ('hong-il', 25, '2019-12-31 00:00:00');
 insert into HERO (name, age, created_date) VALUES ('hong-il', 26, '2019-12-31 00:00:00');
-insert into HERO (name, age, created_date) VALUES ('hong-il', 26, '2019-12-31 00:00:00');
-insert into HERO (name, age, created_date) VALUES ('hong-il', 26, '2019-12-31 00:00:00');
+insert into HERO (name, age, created_date) VALUES ('hong-il', 27, '2019-12-31 00:00:00');
